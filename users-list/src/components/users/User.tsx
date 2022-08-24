3dae8c31f7c61ad757bf6d45439e887a151794fe
matekaszas13 <matekaszas17@gmail.com>
2022-08-24 15:35:06 +0200

    Chore: Add interface for props

--- users-list/src/components/users/User.tsx
+++ users-list/src/components/users/User.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
-const User = () => {
-  return (
-    <div>User</div>
-  )
+interface UserProps {
+  id: number;
+  first_name: string;
+  last_name: string;
+  created_at: string;
 }
 
-export default User
+const User: React.FC<UserProps> = ({
+  id,
+  first_name,
+  last_name,
+  created_at,
+}: UserProps) => {
+  return (
+    <div>
+      <span>{id}</span>
+      <span>{first_name}</span>
+      <span>{last_name}</span>
+      <span>{created_at}</span>
+    </div>
+  );
+};
+
+export default User;
