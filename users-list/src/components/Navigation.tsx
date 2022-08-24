2195ef94e8e359ccb742527e0a5cbd5ef75a9048
matekaszas13 <matekaszas17@gmail.com>
2022-08-24 13:36:50 +0200

    Feat: Add navigation with link to home page

---
+++ users-list/src/components/Navigation.tsx
@@ -0,0 +1,13 @@
+import { Link, useNavigate } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <nav>
+        <div>
+            <Link to={"/"}>Home</Link>
+        </div>
+    </nav>
+  )
+}
+
+export default Navigation
