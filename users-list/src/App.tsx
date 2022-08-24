3b16c03260f984e9a895a86e9f5a3cbe07c43489
matekaszas13 <matekaszas17@gmail.com>
2022-08-24 16:44:30 +0200

    Chore: Add render Users component

--- users-list/src/App.tsx
+++ users-list/src/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import Navigation from "./components/Navigation";
+import Users from "./components/users/Users";
 
 function App() {
   return <div>
     <Navigation/>
+    <Users/>
     <Outlet/>
   </div>;
 }
