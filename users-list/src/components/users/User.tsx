364f5e14c73ecae69bd006346473cfb71c2cc889
matekaszas13 <matekaszas17@gmail.com>
2022-08-26 00:27:09 +0200

    Style: Add basic style for user component

--- users-list/src/components/users/User.tsx
+++ users-list/src/components/users/User.tsx
@@ -23,30 +23,39 @@ const User: React.FC<UserProps> = ({
     const userStatusUpdate = {
       status: userStatus,
     };
-    await datahandler.updateUserStatusByItsId(id, userStatusUpdate)
+    await datahandler.updateUserStatusByItsId(id, userStatusUpdate);
   }
 
   return (
-    <div className="m-4 p-3 border-solid border-2 border-sky-500 rounded-md w-96 h-32">
-      {status === "locked" ? (
-        <h1>
-          <s>
+    <div className="m-4 p-3 border-solid border-2 border-sky-500 rounded-md w-80 h-32">
+      <div className="flex justify-center pb-4">
+        {status === "locked" ? (
+          <h1>
+            <s>
+              {first_name} {last_name}
+            </s>
+          </h1>
+        ) : (
+          <h1>
             {first_name} {last_name}
-          </s>
-        </h1>
-      ) : (
-        <h1>
-          {first_name} {last_name}
-        </h1>
-      )}
-      <h1>{status}</h1>
-      <span>{created_at} {id}</span>
-      <button onClick={() => navigate(`/edit/${id}`)}>Edit</button>
-      <input
-        type="checkbox"
-        checked={status === "locked" ? true : false}
-        onChange={() => updateUserStatus(id)}
-      />
+          </h1>
+        )}
+      </div>
+
+      {/* <h1>{status}</h1> */}
+      <div className="flex flex-col ">
+        <div className="pb-2 flex justify-center">
+          <span>{created_at}</span>
+        </div>
+        <div className="flex justify-center flex-row ">
+          <button className="pr-8" onClick={() => navigate(`/edit/${id}`)}>Edit</button>
+          <input
+            type="checkbox"
+            checked={status === "locked" ? true : false}
+            onChange={() => updateUserStatus(id)}
+          />
+        </div>
+      </div>
     </div>
   );
 };
