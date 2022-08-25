048c0c4fee6846cf451f16466d4a5fb75a847d6c
matekaszas13 <matekaszas17@gmail.com>
2022-08-25 15:27:52 +0200

    Feat: Add createNewUser fetch

--- users-list/src/apis/datahandler.tsx
+++ users-list/src/apis/datahandler.tsx
@@ -1,12 +1,29 @@
+type newUserDetails = {
+  first_name: string;
+  last_name: string;
+  status: string;
+};
 
 export const datahandler = {
-    async getUsers(){
-        const users = await fetch('https://assessment-users-backend.herokuapp.com/users', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        })
-        return await users.json()
-    }
-}
+  async getUsers() {
+    const users = await fetch(
+      "https://assessment-users-backend.herokuapp.com/users",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return await users.json();
+  },
+  async createNewUser(newUserData: newUserDetails) {
+    await fetch("https://assessment-users-backend.herokuapp.com/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newUserData),
+    });
+  },
+};
