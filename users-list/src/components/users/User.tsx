c064d60a6ebafd60fbfe2393cc9c119f5f1b32e5
matekaszas13 <matekaszas17@gmail.com>
2022-08-25 23:54:45 +0200

    Feat: Add display and update status with checkbox

--- users-list/src/components/users/User.tsx
+++ users-list/src/components/users/User.tsx
@@ -6,7 +6,7 @@ interface UserProps {
   first_name: string;
   last_name: string;
   created_at: string;
-  status:string;
+  status: string;
 }
 
 const User: React.FC<UserProps> = ({
@@ -14,19 +14,39 @@ const User: React.FC<UserProps> = ({
   first_name,
   last_name,
   created_at,
-  status
+  status,
 }: UserProps) => {
   const navigate = useNavigate();
 
-
+  async function updateUserStatus(id: number) {
+    const userStatus = status === "locked" ? "active" : "locked";
+    const userStatusUpdate = {
+      status: userStatus,
+    };
+    await datahandler.updateUserStatusByItsId(id, userStatusUpdate)
+  }
 
   return (
     <div className="m-4 p-3 border-solid border-2 border-sky-500 rounded-md">
-      {status === "locked" ? <h1><s>{first_name} {last_name}</s></h1> :<h1>{first_name} {last_name}</h1>}
+      {status === "locked" ? (
+        <h1>
+          <s>
+            {first_name} {last_name}
+          </s>
+        </h1>
+      ) : (
+        <h1>
+          {first_name} {last_name}
+        </h1>
+      )}
       <h1>{status}</h1>
-      <span>{created_at}</span>
+      <span>{created_at} {id}</span>
       <button onClick={() => navigate(`/edit/${id}`)}>Edit</button>
-      <input type="checkbox" />
+      <input
+        type="checkbox"
+        checked={status === "locked" ? true : false}
+        onChange={() => updateUserStatus(id)}
+      />
     </div>
   );
 };
