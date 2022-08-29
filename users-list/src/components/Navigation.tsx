15e09b7dc07adb9b876a0a0051d58ecc42258d3c
matekaszas13 <matekaszas17@gmail.com>
2022-08-29 09:23:58 +0200

    Delete: Delete hidden from className

--- users-list/src/components/Navigation.tsx
+++ users-list/src/components/Navigation.tsx
@@ -3,8 +3,8 @@ import { Link, useNavigate } from "react-router-dom";
 const Navigation = () => {
   return (
     <nav>
-        <div className="p-4 flex justify-between container mx-xl  content-center md:px-0 max-w-[70%] m-auto">
-          <ul className="hidden md:flex text-indigo-900 font-bold tracking-tight">
+        <div className=" p-4 flex justify-between container mx-xl  content-center max-w-[70%] m-auto">
+          <ul className=" md:flex text-indigo-900 font-bold tracking-tight">
             <li className="pl-7"><Link to={"/"}>Home</Link></li>
             <li className="pl-7"><Link to={"/new"}> Add new User</Link></li>
           </ul>
