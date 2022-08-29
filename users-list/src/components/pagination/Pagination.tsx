bb84f3e1a0c5518b2be319849236be7e18e09a42
matekaszas13 <matekaszas17@gmail.com>
2022-08-29 08:27:31 +0200

    Change: give whole user object as prop

--- users-list/src/components/pagination/Pagination.tsx
+++ users-list/src/components/pagination/Pagination.tsx
@@ -1,27 +1,18 @@
 import React, { useEffect, useState } from "react";
+import { IUser } from "../../globals/models";
 import User from "../users/User";
 
-type User = {
-  id: number;
-  last_name: string;
-  first_name: string;
-  status: string;
-  created_at: string;
-  updated_at: string;
-  url: string;
-};
-
-interface paginationProps {
-  users: User[];
+interface PaginationProps {
+  users: IUser[];
 }
 
-const Pagination: React.FC<paginationProps> = ({ users }: paginationProps) => {
+const Pagination: React.FC<PaginationProps> = ({ users}: PaginationProps) => {
   const userPerPage: number = 10;
   const maxNumberOfPages: number = Math.ceil(users.length / userPerPage);
 
   const [currentPage, setCurrentPage] = useState<number>(1);
 
-  const [currentUsers, setCurrentUsers] = useState<User[]>();
+  const [currentUsers, setCurrentUsers] = useState<IUser[]>();
 
   useEffect(() => {
     const begin = (currentPage - 1) * userPerPage;
@@ -39,30 +30,26 @@ const Pagination: React.FC<paginationProps> = ({ users }: paginationProps) => {
   }
 
   return (
-    <div>
-      <div className=" flex flex-row flex-wrap justify-center">
+    <div >
+      <div className="flex flex-row flex-wrap justify-center">
         {currentUsers?.map((user) => (
           <User
-            id={user.id}
-            key={user.id}
-            last_name={user.last_name}
-            first_name={user.first_name}
-            created_at={user.created_at}
-            status={user.status}
+          key={user.id}
+            user={user}
           />
         ))}
       </div>
       <div className="flex justify-between">
       <button
         className="pr-2 mt-2 mr-8 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded"
-        disabled={currentPage === 1 ? true : false}
+        disabled={currentPage === 1}
         onClick={() => handlePrev()}
       >
         prev
       </button>
       <button
       className="mt-2 mr-8 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded"
-        disabled={currentPage === maxNumberOfPages ? true : false}
+        disabled={currentPage === maxNumberOfPages}
         onClick={() => handleNext()}
       >
         next
